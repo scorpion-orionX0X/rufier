708104a3c3e234ed787f1f52cdd56785a775053c
RhemJos <rhemjos@gmail.com>
2025-11-17 18:54:10 -0400

    Mis tareas

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # rufier
+
 latidos del corazón
-## Primera informacion de latidos del corazon 
+
+## Primera informacion de latidos del corazon
+
 LATIDOS POR MINUTO NORMAL SEGUN EDAD (EN REPOSO)
 
 °Bebes: 80-160
 
+## Archivos del proyecto
+
+- **my_app.py** es el archivo principal, desde el cual se ejecuta todo el proyecto.
+- **instr.py** contiene las variables del proyecto.
+- **second_win.py** es el archivo de la segunda ventana.
+- **final_win.py** es el archivo de la ventana.
---
+++ intr.py
@@ -0,0 +1,34 @@
+from PyQt5.QtCore import QTime
+
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
+
+txt_hello = 'Hola...'  # Es el texto de bienvenida
+txt_next = 'Iniciar'  # Es el texto del botón
+txt_instruction = 'Instrucciones ...'  # Instrucciones
+
+txt_title = 'Salud'  # Título
+txt_name = 'Ingresa tu nombre ...'  # Nombre del usuario
+txt_hintname = 'Nombre Completo'  # Nombre completo
+txt_hintage = '0'
+txt_test1 = 'Instrucción 1'  # Instrucciones del paso 1
+txt_test2 = 'Instrucción 2'  # Instrucciones del paso 2
+txt_test3 = 'Instrucción 3'  # Instrucciones del paso 3
+txt_sendresults = 'Enviar los resultados'
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+txt_starttest1 = 'Iniciar el primer examen'  # Editar
+txt_starttest2 = 'Iniciar el segundo paso'  # Editar
+txt_starttest3 = 'Iniciar el examen final'  # Editar
+time = QTime(0, 0, 15)
+txt_timer = time.toString("hh:mm:ss")
+txt_age = 'Edad:'
+txt_finalwin = 'Resultados'
+txt_index = 'Índice de Roufier: '
+txt_workheart = 'Rendimiento cardiaco: '
+txt_res1 = 'Bajo ...'  # Editar
+txt_res2 = 'Satisfactorio ...'  # Editar
+txt_res3 = 'Promedio ...'  # Editar
+txt_res4 = 'Encima del promedio ...'  # Editar
+txt_res5 = 'Alto'
